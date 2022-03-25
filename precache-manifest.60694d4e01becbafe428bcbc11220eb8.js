183b53a7fd3056519cffdcaba499ec0849ba0f9c
ZacharyGopinath <zachary.gopinath@ucc.on.ca>
2022-03-25 09:15:14 -0400

    Updates

---
+++ precache-manifest.60694d4e01becbafe428bcbc11220eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6aeeef5faf8c25e1f956",
+    "url": "/timezones/static/css/main.77935f25.chunk.css"
+  },
+  {
+    "revision": "6aeeef5faf8c25e1f956",
+    "url": "/timezones/static/js/main.6aeeef5f.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/timezones/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "dbdc01f8e7c4b17104cd",
+    "url": "/timezones/static/js/2.dbdc01f8.chunk.js"
+  },
+  {
+    "revision": "f2e9e5c90d054b2d3604",
+    "url": "/timezones/static/js/runtime~main.f2e9e5c9.js"
+  },
+  {
+    "revision": "8f13a2b55a563aae7b5da4c80f568630",
+    "url": "/timezones/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec02ea7e44584cef7f2e0855b0407f43.js"
+  "/timezones/precache-manifest.60694d4e01becbafe428bcbc11220eb8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/timezones/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
